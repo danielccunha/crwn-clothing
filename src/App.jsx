0b7107cd6f264210fb629dd7126ad5568536147a
Daniel Cunha <danielcunha54@gmail.com>
2020-06-16 11:23:19 -0300

    feat: Storing user on Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 
 import Routes from "./routes";
-import { auth } from "./config/firebase";
+import { auth, createUserDocument } from "./config/firebase";
 
 function App() {
   const [currentUser, setCurrentUser] = useState();
 
   useEffect(() => {
     return auth.onAuthStateChanged((user) => {
+      createUserDocument(user);
       return setCurrentUser(user);
     });
   }, []);
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -16,11 +16,31 @@ const config = {
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
+
 export const firestore = firebase.firestore();
+
+export const createUserDocument = async (user, additionalData = {}) => {
+  if (!user) return;
+
+  const userRef = firestore.doc(`users/${user.uid}`);
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = user;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (error) {
+      console.error("Error creating user:", error);
+    }
+  }
+
+  return userRef;
+};
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
-
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
