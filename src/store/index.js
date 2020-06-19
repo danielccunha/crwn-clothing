e86755c285346a7db7246d187bdab73765661ff6
Daniel Cunha <danielcunha54@gmail.com>
2020-06-19 16:32:15 -0300

    feat: Configured redux-persist to persist user's cart

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./ducks/rootReducer";
 
 const middleware = [logger];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
+
+export const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(...middleware))
 );
 
-export default store;
+export const persistor = persistStore(store);
